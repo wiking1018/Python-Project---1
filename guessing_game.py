54df06810549ed15fc3fb13dd5b9e1b1debaf694
PDR <wiking1018@yahoo.co.in>
2021-02-02 21:30:15 -0700

    adds plain number guessing code

--- guessing_game.py
+++ guessing_game.py
@@ -3,9 +3,11 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 
-For this first project we will be using Workspaces. 
+For this first project we will be using Workspaces.
 
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+NOTE: If you strongly prefer to work locally on your own computer, you can
+totally do that by clicking: File -> Download Workspace in the file menu
+after you fork the snapshot of this workspace.
 
 """
 
@@ -14,24 +16,52 @@ import random
 
 def start_game():
     """Psuedo-code Hints
-    
+
     When the program starts, we want to:
     ------------------------------------
     1. Display an intro/welcome message to the player.
     2. Store a random number as the answer/solution.
     3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
+      a. If the guess greater than the solution, display to the player
+         "It's lower".
+      b. If the guess is less than the solution, display to the player
+        "It's higher".
+
     4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
+            and show how many attempts it took them to get the correct number.
+    5. Let the player know the game is ending, or something that indicates
+       the game is over.
     ( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
-
+    print(
+        """
+      ===============================
+      WECLOME TO NUMBER GUESSING GAME
+      ===============================
+      """
+    )
+    random_number = random.randrange(1, 10)
+    guessed_number = int(input("Please enter a Number between 1 - 10 : "))
+    guess_count = 1
+    while True:
+        if guessed_number > random_number:
+            print("Entered Number is greater than guessed Number!")
+            guessed_number = int(input("Please enter a Number between 1 - 10 : "))
+            guess_count += 1
+            continue
+        elif guessed_number < random_number:
+            print("Entered Number is less than guessed Number!")
+            guessed_number = int(input("Please enter a Number between 1 - 10 : "))
+            guess_count += 1
+            continue
+        elif guessed_number == random_number:
+            print("Guessed Number is correct!")
+            break
+    print("number of guesses are :{}".format(guess_count))
 
 
 # Kick off the program by calling the start_game function.
+
+
 start_game()
